e71c0bade4579cc24a7e65b20d5602b8c4a854a6
VictoriaG14 <golden.eagle.elista@gmail.com>
2025-08-28 21:37:58 +0300

    lesson2

---
+++ 05_lesson/lesson05_task2.py
@@ -0,0 +1,10 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("http://uitestingplayground.com/dynamicid")
+blue_button = driver.find_element(By.CSS_SELECTOR, "button.btn-primary")
+blue_button.click()
+sleep(5)
+driver.quit()
